e76a7d694a405ad239918c643ecf209d97e777c1
Bereket Kassahun <beckykassahun@gmail.com>
2025-10-17 22:19:40 +0000

    url bug fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === 'production' ? '' : 'https://conquer-backend.onrender.com';
+const URL = 'https://conquer-backend.onrender.com';
 
 export const socket = io(URL);
